10e9b27a8f0d674a229b9a1b1e6e5f9561200ca8
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-23 12:24:29 +0530

    our first basic server setup with Express Framework

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // this is where we will use the Express Framework to create  a server that listens for incoming requests.
 
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send("Welcome to our new app.")
+})
+
+app.listen(3000)
