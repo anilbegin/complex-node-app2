d9fa5eb787bec730b647edc9c51dcb2a4e682d83
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-23 13:56:18 +0530

    controller example

--- router.js
+++ router.js
@@ -9,8 +9,15 @@ router.get('/', function(req, res) {
 })
 
 router.get('/about', function(req, res) {
-  res.send('This is the about page')
-})
+  res.send('This is the about page') 
+})   // going to localhost:3000/about .. will now show "This is the about page"
+
+router.post('/create-post', postController.create) 
+// someone sends a post request to create post.. 
+// ..then our router can look inside the 'postController' for the 'create' function that we have inside that file
+router.post('/login', userController.login)
+// if a user wants to login, so browser sends a post request to the url of /login ..
+// .. then our router will lokk inside the 'userController' for its function named 'login'
 
 module.exports = router
 
