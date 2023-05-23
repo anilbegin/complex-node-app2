96fa76a835884d0b31fc722a3001101e8b4529dd
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-23 14:14:28 +0530

    initial router, controller setting for form register section

--- router.js
+++ router.js
@@ -1,14 +1,12 @@
 // its the routers job to list out all the url's or routes that we are on the lookout for..
 
 const express = require('express')
-
 const router = express.Router()
 
 const userController = require('./controllers/userController')
 
 router.get('/', userController.home)
-
-
+router.post('/register', userController.register)
 
 module.exports = router
 
