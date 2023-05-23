9b40d0fc2fb361edfa954d6f9f6af2023c01f01c
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-23 14:06:56 +0530

    setup controller for login homepage

--- router.js
+++ router.js
@@ -2,22 +2,13 @@
 
 const express = require('express')
 
-const router = express.Router() 
+const router = express.Router()
 
-router.get('/', function(req, res) {
-    res.render('home-guest')
-})
+const userController = require('./controllers/userController')
 
-router.get('/about', function(req, res) {
-  res.send('This is the about page') 
-})   // going to localhost:3000/about .. will now show "This is the about page"
+router.get('/', userController.home)
 
-router.post('/create-post', postController.create) 
-// someone sends a post request to create post.. 
-// ..then our router can look inside the 'postController' for the 'create' function that we have inside that file
-router.post('/login', userController.login)
-// if a user wants to login, so browser sends a post request to the url of /login ..
-// .. then our router will lokk inside the 'userController' for its function named 'login'
+
 
 module.exports = router
 
