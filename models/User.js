6a631db241c19be84e68be10dcb9be5560e9fb86
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-23 17:34:31 +0530

    comment on why the way to use User.prototype is better than merely creating a function as a proprty inside the User blueprint

--- models/User.js
+++ models/User.js
@@ -3,6 +3,9 @@ let User = function() {
   
 }
 
-User.prototype.jump = function() {}
+User.prototype.jump = function() {} // this way if there are 100s of objects asking for..
+                                  // ..jump function, Javascript will not duplicate the jump..
+                                  // ..function, instead every object will just point towards
+                                  // or jump access this one jump method.
 
 module.exports = User
