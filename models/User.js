5fb268de0458d2bbd6dcecdba2ba64fbbd88a1f7
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-25 12:43:10 +0530

    cleaning up data submitting by user during registration

--- models/User.js
+++ models/User.js
@@ -6,6 +6,19 @@ let User = function(data) {
   this.errors = []
 }
 
+User.prototype.cleanUp = function() {
+  if(typeof(this.data.username) != "string") {this.data.username = ""}
+  if(typeof(this.data.email) != "string") {this.data.username = ""}
+  if(typeof(this.data.password) != "string") {this.data.username = ""}
+
+  // get rid of any bogus properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password
+  }
+}
+
 User.prototype.validate = function() {
   if(this.data.username == "") {this.errors.push("You must provide a username.")}
   if(this.data.username != "" && !validator.isAlphanumeric(this.data.username))  {this.errors.push('Username can only contain letters and numbers')}
@@ -21,6 +34,7 @@ User.prototype.validate = function() {
 
 User.prototype.register = function() {
   // step 1. Validate User data
+  this.cleanUp()
   this.validate()
   // step 2. Only if there are no Validation errors, then save the user data into a database
 }
