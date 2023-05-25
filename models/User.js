b2a9b4583192583576c5cb644d3a1e2a1ac17c69
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-25 12:25:53 +0530

    more User data Validation -1

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const validator = require('validator')
+
 // Constructor
 let User = function(data) {
   this.data = data
@@ -6,8 +8,15 @@ let User = function(data) {
 
 User.prototype.validate = function() {
   if(this.data.username == "") {this.errors.push("You must provide a username.")}
-  if(this.data.email == "") {this.errors.push("You must provide a valid email address.")}
+  if(this.data.username != "" && !validator.isAlphanumeric(this.data.username))  {this.errors.push('Username can only contain letters and numbers')}
+  if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
   if(this.data.password == "") {this.errors.push("You must provide a password.")}
+
+  if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push('Username must be atleast 3 characters')}
+  if(this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters")}
+  if(this.data.password.length > 0 && this.data.password.length < 6) {this.errors.push('Password must be atleast 6 characters')}
+  if(this.data.password.length > 50) {this.errors.push("Passwword cannot exceed 50 characters")}
+
 }
 
 User.prototype.register = function() {
