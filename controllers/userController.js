47146e25dc777be78f2fc66e72da156fd539ab04
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-31 18:10:10 +0530

    1: flash message for basic validation & unique registration inputs

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,13 @@ exports.register = function(req, res) {
                 //new User(req.body) -here we are just passing form field values that user just submitted to the new User Object
   user.register()
   if(user.errors.length) {
-    res.send(user.errors)
+  //  res.send(user.errors)
+  user.errors.forEach(function(err) {
+    req.flash('regErr', err)
+  })
+  req.session.save(function() {
+    res.redirect('/')
+  })
   } else {
     res.send('Congrats there are no errors')
   } 
@@ -40,6 +46,6 @@ exports.home = function(req, res) {
   if(req.session.user) {
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest', {errors: req.flash('errors')})
+    res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErr')})
   }
 }
--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ User.prototype.cleanUp = function() {
   }
 }
 
-User.prototype.validate = function() {
+User.prototype.validate = async function() {
   if(this.data.username == "") {this.errors.push("You must provide a username.")}
   if(this.data.username != "" && !validator.isAlphanumeric(this.data.username))  {this.errors.push('Username can only contain letters and numbers')}
   if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
@@ -33,6 +33,16 @@ User.prototype.validate = function() {
   if(this.data.password.length > 0 && this.data.password.length < 6) {this.errors.push('Password must be atleast 6 characters')}
   if(this.data.password.length > 50) {this.errors.push("Password cannot exceed 50 characters")}
 
+  // Only if username is valid then check to see if its already taken
+  if(this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+    let usernameExists = await usersCollection.findOne({username: this.data.username})
+    if(usernameExists) this.errors.push("that username is already taken")
+  }
+  // Only if username is valid then check to see if its already taken
+  if(validator.isEmail(this.data.email)) {
+    let emailExists = await usersCollection.findOne({email: this.data.email})
+    if(emailExists) this.errors.push("that email is already being used")
+  }
 }
 
 User.prototype.register = function() {
