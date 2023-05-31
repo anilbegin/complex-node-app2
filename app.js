b8dcf4a934be49a1833f0b652cc9d1ed9cd73438
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-05-31 12:11:47 +0530

    inital setup to test Sessions

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // this is where we will use the Express Framework to create  a server that listens for incoming requests.
 
 const express = require('express')
+const session = require('express-session')
 const app = express()
+
+// configuration options for session
+let sessionOptions = session({
+   secret: "Javascript secret waystar",
+   resave: false,
+   saveUninitialized: false,
+   cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}  
+})
+// maxAge: 1000milliseconds = 1second,* 60 = 1minute,*60 = 1hour,*24 = 1day,
+// so here, cookie expires in 1 day.
+
+app.use(sessionOptions)
 
 const router = require('./router')
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models/User')
 exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(function(result) {
+    req.session.user = {favColor: "blue", username: user.data.username}
     res.send(result)
   }).catch(function(err) {
     res.send(err)
@@ -25,5 +26,9 @@ exports.register = function(req, res) {
 }
 
 exports.home = function(req, res) {
-  res.render('home-guest')
+  if(req.session.user) {
+    res.send("Welcome to the actual application " + req.session.user.favColor)
+  } else {
+    res.render('home-guest')
+  }
 }
