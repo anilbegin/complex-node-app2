bf2a99dbff049065f3559fe4ecc97af1312830c9
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-21 18:27:10 +0530

    inital steps before adding the Search template

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,23 @@
 export default class Search {
+  // 1. Select dom elements and keep track of any useful data
   constructor() {
-    alert("Search js is successfully being executed")
+    this.headerSearchIcon = document.querySelector(".header-search-icon")
+    this.events()
+  }
+
+  // 2. Events
+  events() {
+    this.headerSearchIcon.addEventListener('click', e => {
+      e.preventDefault()
+      this.openOverlay()
+    })
+  }
+
+  // 3. Methods
+  openOverlay() {
+    alert("Open overlay method just ran")
   }
 }
+
+
+  
