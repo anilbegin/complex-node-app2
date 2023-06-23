94234941a2ea15fa60959fe9775daa4cb14791b3
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-23 19:28:15 +0530

    working on the stop following button

--- models/Follow.js
+++ models/Follow.js
@@ -13,7 +13,7 @@ Follow.prototype.cleanUp = function() {
   if(typeof(this.followedUsername) != 'string') this.followedUsername = ""
 }
 
-Follow.prototype.validate = async function() {
+Follow.prototype.validate = async function(action) {
   // followedUsername must exist in Database
   let followedAccount = await usersCollection.findOne({username: this.followedUsername})
   if(followedAccount) {
@@ -22,14 +22,35 @@ Follow.prototype.validate = async function() {
   } else {
     this.errors.push("you cannot follow a user that does not exist")
   }
+
+  let doesFollowAlreadyExist = await followsCollection.findOne({followedId: this.followedId, authorId: new ObjectId(this.authorId)})
+  if(action == "create") {
+    if(doesFollowAlreadyExist) this.errors.push("you are already following this user.")
+  }
+  if(action == "delete") {
+    if(!doesFollowAlreadyExist) this.errors.push("you are cannot stop following someone, you do not already follow.")
+  }
 }
 
 Follow.prototype.create = function() {
   return new Promise(async (resolve, reject) => {
     this.cleanUp()
-   await this.validate()
+   await this.validate("create")
     if(!this.errors.length) {
      await followsCollection.insertOne({followedId: this.followedId, authorId: new ObjectId(this.authorId)})
+     resolve() 
+    } else {
+      reject(this.errors)
+    }
+  })
+}
+
+Follow.prototype.delete = function() {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp()
+   await this.validate("delete")
+    if(!this.errors.length) {
+     await followsCollection.deleteOne({followedId: this.followedId, authorId: new ObjectId(this.authorId)})
      resolve() 
     } else {
       reject(this.errors)
