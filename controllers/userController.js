6bc52ab71f597ea5c7a4d0cbbe45c68a59c0d10b
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-24 14:18:17 +0530

    highlighting the tab that is Active - posts,followers,following Tab

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,6 +90,7 @@ exports.profilePostsScreen = function(req, res) {
   // ask our post model for posts by a cerain authorID
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
     res.render('profile', {
+    currentPage: "posts",  
       posts: posts,
       profileUsername: req.profileUser.username,
       profileAvatar: req.profileUser.avatar,
@@ -105,6 +106,7 @@ exports.profileFollowersScreen = async function(req, res) {
   try {
     let followers = await Follow.getFollowersById(req.profileUser._id)
     res.render('profile-followers', {
+    currentPage: "followers",  
     followers: followers,
       profileUsername: req.profileUser.username,
       profileAvatar: req.profileUser.avatar,
@@ -120,6 +122,7 @@ exports.profileFollowingScreen = async function(req, res) {
   try {
     let following = await Follow.getFollowingById(req.profileUser._id)
     res.render('profile-following', {
+    currentPage: "following",
     following: following,
       profileUsername: req.profileUser.username,
       profileAvatar: req.profileUser.avatar,
