977c8ba8a490203f3f692e305bc5f71af13a69d5
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-26 12:30:12 +0530

    remove console.log line

--- controllers/userController.js
+++ controllers/userController.js
@@ -84,7 +84,6 @@ exports.home = async function(req, res) {
   if(req.session.user) {
     // fetch feed of posts for current User, i.e the posts that are created by accounts that this user follows
     let posts = await Post.getFeed(req.session.user._id)
-    console.log(posts)
     res.render('home-dashboard', {posts: posts})
   } else {
     res.render('home-guest', {regErrors: req.flash('regErr')})
