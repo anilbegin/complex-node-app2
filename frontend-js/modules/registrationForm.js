78926d4486030d75e8c9a269302c8e83ec11faed
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-28 13:53:22 +0530

    triggering a function (usernameHandler) on a valid key stroke

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -2,18 +2,35 @@ export default class RegistrationForm {
   constructor() {
     this.allFields = document.querySelectorAll("#registration-form .form-control")
     this.insertValidationElements()
+    this.username = document.querySelector("#username-register")
+    this.username.previousValue = ""
     this.events()
   }
 
   // Events
   events() {
-    alert("registration form js is running")
+    this.username.addEventListener('keyup', () => {
+      this.isDifferent(this.username, this.usernameHandler)
+    })
   } 
 
   // Methods
+  // isDifferent -  check if the key press is actually a character and not the arrow key, capslock, end, Tab, etc
+  isDifferent(el, handler) {
+    if(el.previousValue != el.value) {
+      handler.call(this)
+    }
+    el.previousValue = el.value
+  } 
+
+  // usernameHandler -  triggered by isDifferent function
+  usernameHandler() {
+    alert("Username handler just ran")
+  }
+
   insertValidationElements() {
     this.allFields.forEach(function(el) {
-      el.insertAdjacentHTML('afterend', '<div class="alert alert-danger small liveValidateMessage liveValidateMessage--visible">hello</div>')
+      el.insertAdjacentHTML('afterend', '<div class="alert alert-danger small liveValidateMessage"></div>')
     })
   }
 }
