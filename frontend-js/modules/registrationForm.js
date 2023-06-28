debf250eef89653af07fa73d36f8e273a38eef49
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-06-28 11:44:19 +0530

    create a module skeleton for adding regstration form frontend validation

---
+++ frontend-js/modules/registrationForm.js
@@ -0,0 +1,12 @@
+export default class RegistrationForm {
+  constructor() {
+    this.events()
+  }
+
+  // Events
+  events() {
+    alert("registration form js is running")
+  } 
+
+  // Methods
+}
