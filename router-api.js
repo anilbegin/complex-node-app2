767968109ab661c3d0c96619a3c86755cb895366
anilbegin <98578895+anilbegin@users.noreply.github.com>
2023-07-05 13:26:50 +0530

    fecthing all posts from a particular user, using api - testing with POSTMAN

--- router-api.js
+++ router-api.js
@@ -3,8 +3,11 @@ const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 const followController = require('./controllers/followController')
 
+
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
 apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete)
+apiRouter.get('/postsByAuthor/:username', userController.apiGetPostsByUsername)
 
 module.exports = apiRouter
+
